d7714555e43b97464c93c44c6f41262464f46d31
keyunjie96 <kyj14@mails.tsinghua.edu.cn>
2018-11-09 20:00:19 +0800

    [fix]repo url

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ wrapper() {
   BLUE="\033[0;36m"
   NORMAL="\033[0m"
 
-  REPO_HTTPS="https://github.com/ets-labs/python-vimrc.git"
-  VUNDLE_HTTPS="https://github.com/VundleVim/Vundle.vim.git"
+  REPO_HTTPS="https://github.com/keyunjie96/python-vimrc.git"
 
 echo "${BLUE}"
 cat << "HELLO_TEXT"
